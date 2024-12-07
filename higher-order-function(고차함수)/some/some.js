afa849cbeb4f0477a09115a4bfc6ecc487b62ebc
KimYoungHwan8750 <dnfwlq123@naver.com>
2024-12-07 11:58:24 +0900

    고차함수 some 정리

---
+++ higher-order-function(고차함수)/some/some.js
@@ -0,0 +1,10 @@
+const list = [1, 2, 3, 4, 5]
+const result = list.some((item, index, original) => {
+  console.log('item',item)
+  if(item === 3) {
+    return false
+  } else if (item === 4) {
+    return true
+  }
+})
+console.log('result',result)
